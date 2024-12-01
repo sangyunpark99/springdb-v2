0c77646de6e796beee62a14637d32f68be268aa1
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-12-01 19:23:05 +0900

    섹션3 데이터 접근 기술 - 테스트
    테스트 - 스프링 부트와 임베디드 모드

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -29,18 +29,19 @@ public class ItemServiceApplication {
 		return new TestDataInit(itemRepository);
 	}
 
-	@Bean
-	@Profile("test") // Profile이 test인 경우
-	public DataSource dataSource() {
-		log.info("메모리 데이터베이스 초기화");
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("org.h2.Driver"); // h2 database 드라이버 지정
-		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1"); // database 연결이 닫혀도 데이터베이스가 삭제되지 않도록 제어
-		// db;DB_CLOSE_DELAY=-1 ; 사용하는 부분을 유의해야 한다.
-		// mem:db => 임베디드 모드(메모리 모드)로 동작하는 H2 데이터베이스를 사용할 수 있다.
-		dataSource.setUsername("sa");
-		dataSource.setPassword("");
-		return dataSource;
-	}
+	// 아래처럼 빈으로 등록을 하지 않아도 된다.
+//	@Bean
+//	@Profile("test") // Profile이 test인 경우
+//	public DataSource dataSource() {
+//		log.info("메모리 데이터베이스 초기화");
+//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//		dataSource.setDriverClassName("org.h2.Driver"); // h2 database 드라이버 지정
+//		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1"); // database 연결이 닫혀도 데이터베이스가 삭제되지 않도록 제어
+//		// db;DB_CLOSE_DELAY=-1 ; 사용하는 부분을 유의해야 한다.
+//		// mem:db => 임베디드 모드(메모리 모드)로 동작하는 H2 데이터베이스를 사용할 수 있다.
+//		dataSource.setUsername("sa");
+//		dataSource.setPassword("");
+//		return dataSource;
+//	}
 
 }
