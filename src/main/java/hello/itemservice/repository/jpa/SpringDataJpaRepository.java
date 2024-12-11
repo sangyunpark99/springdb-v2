7553e9ba02b80cdea61381bb1586e7515a1f1871
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-12-11 23:35:41 +0900

    섹션6 데이터 접근 기술 - JPA
    스프링 데이터 JPA 적용1

---
+++ src/main/java/hello/itemservice/repository/jpa/SpringDataJpaRepository.java
@@ -0,0 +1,26 @@
+package hello.itemservice.repository.jpa;
+
+import hello.itemservice.domain.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface SpringDataJpaRepository extends JpaRepository<Item, Long> {
+
+    // 조건 없이 검색하는건 Spring Data Jpa가 기본적으로 CRUD 기능이 제공된다.
+
+    List<Item> findByItemNameLike(String itemName);
+
+    List<Item> findByPriceLessThanEqual(Integer price);
+
+    // 이름이 너무 길다
+    List<Item> findByItemNameLikeAndPriceLessThanEqual(String itemName, Integer price);
+
+    // 이름이 너무 길어진 경우 아래와 같이 @Query를 사용한다.
+    // 실무에서 쿼리가 너무 복잡한 경우 Querydsl을 사용하면 된다.
+    @Query("select i from Item i where i.itemName like :itemName and i.price <= :price")
+    List<Item> findItems(@Param("itemName") String itemName, @Param("price") Integer price);
+
+}
