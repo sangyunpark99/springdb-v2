7d5d59c8b1b7f661a6745ccc1396bd7163e95998
psangyun5108@gmail.com <psangyun5108@gmail.com>
2025-02-14 09:06:28 +0900

    섹션12 트랜잭션의 전파 활용4 - 전파 커밋

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -1,6 +1,7 @@
 package hello.springtx.propagation;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,6 +28,35 @@ class MemberServiceTest {
      */
     @Test
     void outerTxOff_success() {
+        // given
+        String username = "outerTxOff_success";
+
+        // when
+        memberService.joinV1(username);
+
+        // then 데이터 정상 실행
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
+
+    /**
+     * memberService @Transactional: OFF
+     * memberRepository @Transactional: ON
+     * logRepository @Transactional : ON Exception
+     */
+    @Test
+    void outerTxOff_fail() { // 회원 저장, 로그는 롤백
+        // given
+        String username = "로그예외";
+
+        // when, then
+        Assertions.assertThatThrownBy(() -> memberService.joinV1(username)).isInstanceOf(RuntimeException.class);
+        assertTrue(memberRepository.find(username).isPresent()); // 저장
+        assertTrue(logRepository.find(username).isEmpty()); // 롤백
+    }
+
+    @Test
+    void singleTx() {
         // given
         String username = "outerTxOff_success";
 
